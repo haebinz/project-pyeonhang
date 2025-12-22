2697a2d6473311ac63654c1f08163ecc831ce429
boeun <supbe0726@naver.com>
2025-12-22 19:15:36 +0900

    .

--- pyeonhang/src/main/java/projecct/pyeonhang/common/filter/JWTFilter.java
+++ pyeonhang/src/main/java/projecct/pyeonhang/common/filter/JWTFilter.java
@@ -2,6 +2,8 @@ package projecct.pyeonhang.common.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,30 +11,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 import projecct.pyeonhang.common.utils.JWTUtils;
 import projecct.pyeonhang.users.dto.UserSecureDTO;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
 public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtils jwtUtils;
 
+     private static final List<String> PERMIT_PATHS = List.of("/api/v1/user/login", "/api/v1/user/add", "/api/v1/board", "/api/v1/crawl/**", "/api/v1/refresh", "/api/v1/email", "/api/v1/email/**", "/favicon.ico");
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getRequestURI();
+        if ("OPTIONS".equals(request.getMethod())) {
+        filterChain.doFilter((ServletRequest)request, (ServletResponse)response);
+        return;
+        } 
+
+        if (isPermitPath(path)) {
+        filterChain.doFilter((ServletRequest)request, (ServletResponse)response);
+        return;
+        } 
+        
         //요청 request header 에서 token 찾기
         //헤더에 원래 있는 속성임....
         String acessToken = request.getHeader("Authorization");
 
-        if(acessToken == null) {
-            log.info("acessToken is null");
-            filterChain.doFilter(request, response);
-            return; // 자격이 없으니 로그인으로 넘거가라!
+        if(acessToken == null || !acessToken.startsWith("Bearer ")) {
+            response.setStatus(401);
+            return;
         }
 
         try{
@@ -45,20 +61,15 @@ public class JWTFilter extends OncePerRequestFilter {
                 }
             }
 
-            //토큰의 카테고리 검색
-            String category = jwtUtils.getCategory(acessToken);
-
-            if(! category.equals("access")) {
-                throw new IllegalAccessException("유효하지 않은 토큰입니다.");
-            }
-
-        }catch(Exception e) {
-            //response 한다!
+            String category = this.jwtUtils.getCategory(acessToken);
+            if (!category.equals("access"))
+                throw new IllegalAccessException("유효하지 않은 토큰입니다."); 
+        } catch (Exception e) {
             PrintWriter writer = response.getWriter();
-            writer.println("토큰이 유효하지 않습니다.");
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;  // 함수 종료
-        }
+            writer.println("유효하지 않은 토큰입니다.");
+            response.setStatus(401);
+            return;
+        } 
 
         // 인증 성공
         String userId = jwtUtils.getUserId(acessToken);
@@ -80,6 +91,13 @@ public class JWTFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
 
     }
+
+
+    private boolean isPermitPath(String path) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        return PERMIT_PATHS.stream()
+        .anyMatch(pattern -> matcher.match(pattern, path));
+    }    
 
 
 }
